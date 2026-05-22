60ba58589775196cd558bd7493ffefd5886f7d39
CristianoPMorais <57453549+CristianoPMorais@users.noreply.github.com>
2026-05-22 11:49:34 -0300

    Add files via upload

---
+++ api/server.js
@@ -0,0 +1,8 @@
+import { createClient } from '@redis/client';
+
+const client = createClient({
+    url: process.env.REDIS_URL
+});
+
+client.on('error', err => console.log('Redis Client Error', err));
+await client.connect();
